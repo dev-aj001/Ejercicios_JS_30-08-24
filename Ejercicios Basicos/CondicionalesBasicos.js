9e0cad3ac4758b9b38ae41853d1e2279111aa5dd
Armando Jair Ibañez Parra <jairibaparr@outlook.com>
2024-08-31 13:16:58 -0700

    Ejercicios Basicos
    Se agrega la carpeta con los ejercicios básicos de JavaScript

---
+++ Ejercicios Basicos/CondicionalesBasicos.js
@@ -0,0 +1,9 @@
+function condicionales(edad){
+    if(edad>=18){
+        console.log("Eres mayor de edad");
+    }
+    else {
+        console.log("Eres menor de edad");
+    }
+}
+condicionales(19);
