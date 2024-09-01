4cf4c04ec5dc38ab4ecc1bb440866f9c0c8f3390
Cantabrana2021 <90821343+Cantabrana2021@users.noreply.github.com>
2024-09-01 11:42:43 -0600

    Adición de ejercicios avanzados
    By López Cantabrana Christian Isabel 19400606

---
+++ Ejercicios Avanzados/AsyncAwait.js
@@ -0,0 +1,5 @@
+const result = await asyncFunction();
+const result2 = await f1(result);
+return await f2(result2);
+
+await promesaSuma(5,6);
---
+++ Ejercicios Avanzados/ManipulacionComplejaArray.js
@@ -0,0 +1,5 @@
+let numeros = [11,12,13,1,4,2]
+function duplicaryFiltrar(array){
+    return array.filter(x=> x>10)
+}
+console.log(duplicaryFiltrar(numeros));
